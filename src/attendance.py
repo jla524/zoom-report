5368d29a0e3ffb106b7e1c1bcd9b6b036d31eb4c
Jacky Lee <jla524@sfu.ca>
2022-01-15 21:14:52 -0800

    Improve variable naming and fix bugs

--- src/attendance.py
+++ src/attendance.py
@@ -1,53 +1,55 @@
-from typing import Dict
 from datetime import datetime
 import pandas as pd
 from api.zoom import Zoom
 from config import Config
 
 
-def get_participants(meeting_id):
+def get_attendance(meeting_id) -> dict:
     zoom = Zoom(meeting_id)
-    response = zoom.get_meeting_participants()
-    participants = response.json().get('participants')
-    while token := response.json().get('next_page_token'):
-        response = zoom.get_meeting_participants(next_page_token=token)
-        participants += response.json().get('participants')
+    response = zoom.get_participants()
+    participants = response.get('participants')
+    while token := response.get('next_page_token'):
+        response = zoom.get_participants(next_page_token=token)
+        participants += response.get('participants')
     return participants
 
 
-def convert_to_frame(participants):
-    df = pd.DataFrame(participants)
+def convert_to_frame(info) -> pd.DataFrame:
+    frame = pd.DataFrame(info)
     for column in ['join_time', 'leave_time']:
-        df[column] = pd.to_datetime(df[column]) \
+        frame[column] = pd.to_datetime(frame[column]) \
             .dt.tz_convert(Config.timezone())
-    df.sort_values(['id', 'name', 'join_time'], inplace=True)
-    return df
+    frame.sort_values(['id', 'name', 'join_time'], inplace=True)
+    return frame
 
 
-def combine_rejoins(df):
-    output_df = df.groupby(['id', 'name', 'user_email']) \
-        .agg({"duration": ['sum'], 'join_time': ['min'], 'leave_time': ['max']}) \
+def combine_rejoins(frame):
+    frame = frame.groupby(['id', 'name', 'user_email']) \
+        .agg({'duration': ['sum'],
+              'join_time': ['min'],
+              'leave_time': ['max']}) \
         .reset_index() \
         .rename(columns={"duration": "total_duration"})
-    output_df.columns = output_df.columns.get_level_values(0)
-    output_df.total_duration = round(output_df.total_duration / 60, 2)
-    return output_df
+    frame.columns = frame.columns.get_level_values(0)
+    frame.total_duration = round(frame.total_duration / 60, 2)
+    return frame
 
 
-def save_attendence(output_df, id):
+def save_report(df, meeting_id):
     date = datetime.today().date()
     output_dir = Config.output_dir()
     output_dir.mkdir(exist_ok=True)
-    output_file = output_dir / f'{id}_{date}.csv'
-    output_df.to_csv(output_file, index=False)
+    output_file = output_dir / f'{meeting_id}_{date}.csv'
+    df.to_csv(output_file, index=False)
+    print("File writtten to " + str(output_file))
     return output_file
 
 
 def fetch(meeting_id):
-    data = get_participants(meeting_id)
-    if data is None:
+    attendance = get_attendance(meeting_id)
+    if not attendance:
         print("Attendance data is not available")
-        return
-    data = convert_to_frame(data)
-    data = combine_rejoins(data)
-    return save_attendence(data, meeting_id)
+        return None
+    attendance = convert_to_frame(attendance)
+    attendance = combine_rejoins(attendance)
+    return save_report(attendance, meeting_id)
