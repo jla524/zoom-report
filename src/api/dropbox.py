10c65d2c4e52ad9723b70b875ba449139c0da4f6
Jacky Lee <jla524@sfu.ca>
2022-01-16 16:25:43 -0800

    Use calls from pathlib

--- src/api/dropbox.py
+++ src/api/dropbox.py
@@ -1,13 +1,14 @@
 import dropbox
+from pathlib import Path
 
 
 class TransferData:
     def __init__(self, access_token):
         self.access_token = access_token
 
-    def upload_file(self, file_from, file_to):
+    def upload_file(self, file_from: Path, file_to: Path):
         """upload a file to Dropbox using API v2"""
         dbx = dropbox.Dropbox(self.access_token)
 
-        with open(file_from, 'rb') as file:
-            dbx.files_upload(file.read(), file_to)
+        with file_from.open('rb') as file:
+            dbx.files_upload(file.read(), str(file_to))
