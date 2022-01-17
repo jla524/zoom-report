27251e8a9862cb02b6a9e3582a2ed57cf8ff6eb1
Jacky Lee <jla524@sfu.ca>
2022-01-17 00:53:49 -0800

    Revert "Move fetch into main.py"
    This reverts commit df34a600ddc603b721c9b25d0da5d796ab85e514.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
 import sys
-from pathlib import Path
-from storage import upload_file
-from attendance import get_info, convert_to_frame, combine_rejoins, save_report
-from meetings import get_instances, get_uuids
+import attendance
+import storage
 from logger.pkg_logger import Logger
-
-
-def fetch_meeting_attendance(_id) -> Path:
-    info = get_info(_id)
-    if not info:
-        Logger.info("Attendance data is not available.")
-        return None
-    info = convert_to_frame(info)
-    info = combine_rejoins(info)
-    return save_report(info, _id)
 
 
 if __name__ == "__main__":
@@ -22,5 +10,5 @@ if __name__ == "__main__":
         Logger.error("Please enter a meeting id")
         sys.exit(1)
     meeting_id = sys.argv[1]
-    file_path = fetch_meeting_attendance(meeting_id)
-    upload_file(file_path)
+    file_path = attendance.fetch(meeting_id)
+    storage.upload_file(file_path)
