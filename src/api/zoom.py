f21c29ef6965c0c430b192445f3bad52ebdba762
Jacky Lee <jla524@sfu.ca>
2022-01-17 15:54:25 -0800

    Remove method type hints, will add docstrings later

--- src/api/zoom.py
+++ src/api/zoom.py
@@ -9,7 +9,7 @@ from logger.pkg_logger import Logger
 class Zoom:
     _base_url = 'https://api.zoom.us/v2/.'
 
-    def _send_request(self, route, params=None) -> dict:
+    def _send_request(self, route, params=None):
         url = urljoin(self._base_url, route)
         Logger.info(f"Sending request to {url}")
         token = Config.zoom_jwt_token()
@@ -22,17 +22,17 @@ class Zoom:
             response = requests.get(url, headers=headers, params=params)
         return response.json()
 
-    def get_meeting_instances(self, meeting_id) -> dict:
+    def get_meeting_instances(self, meeting_id):
         Logger.info("Retrieving meeting instances...")
         route = f'past_meetings/{meeting_id}/instances'
         return self._send_request(route)
 
-    def get_meeting_details(self, meeting_id) -> dict:
+    def get_meeting_details(self, meeting_id):
         Logger.info("Retrieving meeting details...")
         route = f'meetings/{meeting_id}'
         return self._send_request(route)
 
-    def get_participants(self, meeting_id, next_page_token=None) -> dict:
+    def get_participants(self, meeting_id, next_page_token=None):
         Logger.info("Retrieving meeting participants...")
         route = f'report/meetings/{meeting_id}/participants'
         params = {'page_size': 300}
@@ -40,9 +40,10 @@ class Zoom:
             params.update({'next_page_token': next_page_token})
         return self._send_request(route, params)
 
-    def get_past_participants(self, meeting_uuid: str) -> dict:
+    def get_past_participants(self, meeting_uuid):
         Logger.info("Retrieving past meeting participants...")
         if meeting_uuid.startswith('/') or '//' in meeting_uuid:
             meeting_uuid = quote_plus(quote_plus(meeting_uuid))
         route = f'past_meetings/{meeting_uuid}/participants'
-        return self._send_request(route)
+        params = {'page_size': 300}
+        return self._send_request(route, params)
