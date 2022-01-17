5a13a60362c4a5e51bbfdb8ffeba7049790f55d5
Jacky Lee <jla524@sfu.ca>
2022-01-17 10:31:48 -0800

    Restructure Zoom API calls and add log info

--- src/api/zoom.py
+++ src/api/zoom.py
@@ -9,11 +9,9 @@ from logger.pkg_logger import Logger
 class Zoom:
     _base_url = 'https://api.zoom.us/v2/.'
 
-    def __init__(self, meeting_id: str):
-        self._meeting_id = meeting_id
-
     def _send_request(self, route, params=None) -> dict:
         url = urljoin(self._base_url, route)
+        Logger.info(f"Sending request to {url}")
         token = Config.zoom_jwt_token()
         headers = {'Authorization': f'Bearer {token}'}
         response = requests.get(url, headers=headers, params=params)
@@ -24,22 +22,26 @@ class Zoom:
             response = requests.get(url, headers=headers, params=params)
         return response.json()
 
-    def get_meeting_instances(self) -> dict:
-        route = f'past_meetings/{self._meeting_id}/instances'
+    def get_meeting_instances(self, meeting_id) -> dict:
+        Logger.info("Retrieving meeting instances...")
+        route = f'past_meetings/{meeting_id}/instances'
         return self._send_request(route)
 
-    def get_meeting_details(self) -> dict:
-        route = f'meetings/{self._meeting_id}'
+    def get_meeting_details(self, meeting_id) -> dict:
+        Logger.info("Retrieving meeting details...")
+        route = f'meetings/{meeting_id}'
         return self._send_request(route)
 
-    def get_participants(self, next_page_token=None) -> dict:
-        route = f'report/meetings/{self._meeting_id}/participants'
+    def get_participants(self, meeting_id, next_page_token=None) -> dict:
+        Logger.info("Retrieving meeting participants...")
+        route = f'report/meetings/{meeting_id}/participants'
         params = {'page_size': 300}
         if next_page_token:
             params.update({'next_page_token': next_page_token})
         return self._send_request(route, params)
 
     def get_past_participants(self, meeting_uuid: str) -> dict:
+        Logger.info("Retrieving past meeting participants...")
         if meeting_uuid.startswith('/') or '//' in meeting_uuid:
             meeting_uuid = quote_plus(quote_plus(meeting_uuid))
         route = f'past_meetings/{meeting_uuid}/participants'
