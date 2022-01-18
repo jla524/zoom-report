cee2bbd20f8070e3d963343ddc2aea63e1a83426
Jacky Lee <jla524@sfu.ca>
2022-01-17 22:49:23 -0800

    Improve error handling with response status

--- src/api/zoom.py
+++ src/api/zoom.py
@@ -1,4 +1,4 @@
-from urllib.parse import urljoin, quote_plus
+from urllib.parse import urljoin
 import requests
 from config import Config
 from common.enums import Http
@@ -15,22 +15,24 @@ class Zoom:
         token = Config.zoom_jwt_token()
         headers = {'Authorization': f'Bearer {token}'}
         response = requests.get(url, headers=headers, params=params)
-        if response.status_code != Http.OK:
+        if response.status_code == Http.UNAUTHORIZED:
             Logger.info("The token has expired. Requesting a new token.")
             new_token = renew_jwt_token()
             headers = {'Authorization': f'Bearer {new_token}'}
             response = requests.get(url, headers=headers, params=params)
-        return response.json()
+        elif response.status_code != Http.OK:
+            Logger.info("An error has occured when sending request.")
+        return response
 
     def get_meeting_details(self, meeting_id):
         Logger.info("Retrieving meeting details...")
         route = f'meetings/{meeting_id}'
-        return self._send_request(route)
+        return self._send_request(route).json()
 
     def get_meeting_instances(self, meeting_id):
         Logger.info("Retrieving meeting instances...")
         route = f'past_meetings/{meeting_id}/instances'
-        return self._send_request(route)
+        return self._send_request(route).json()
 
     def get_participants(self, meeting_uuid, next_page_token=None):
         Logger.info("Retrieving meeting participants...")
@@ -38,4 +40,8 @@ class Zoom:
         params = {'page_size': 300}
         if next_page_token:
             params.update({'next_page_token': next_page_token})
-        return self._send_request(route, params)
+        response = self._send_request(route, params)
+        if response.status_code == Http.NOT_FOUND:
+            Logger.error("Unable to retrieve meeting {meeting_uuid}")
+            return None
+        return response.json()
