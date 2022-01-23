5fb5fe4fb91e020a5d833f0265903663f4fe2c03
Jacky Lee <jla524@sfu.ca>
2022-01-22 22:24:46 -0800

    Get recent instances with -n flag

--- zoom_report/main.py
+++ zoom_report/main.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from zoom_report import Config
 from zoom_report.command_line import Cli
 from zoom_report.api.zoom import Zoom
@@ -6,7 +7,7 @@ from zoom_report.automate.attendance import get_report
 from zoom_report.automate.storage import save_report
 
 
-def get_localized_instances(instances: list[dict]) -> list[tuple]:
+def localize_instances(instances: dict) -> list[tuple]:
     if instances and 'meetings' in instances:
         instances = [(meeting['uuid'], localize(meeting['start_time']))
                      for meeting in instances['meetings']]
@@ -14,15 +15,29 @@ def get_localized_instances(instances: list[dict]) -> list[tuple]:
     return instances
 
 
-def process_reports(meeting_id) -> None:
+def filter_instances(instances: list[tuple], days: int = 3) -> list[tuple]:
+    if instances:
+        date_format = Config.datetime_format().split(' ', maxsplit=1)[0]
+        start_date = (date.today() - timedelta(days=days)) \
+            .strftime(date_format)
+        index = len(instances) - 1
+        while instances[index][1] > start_date:
+            index -= 1
+        instances = instances[index+1:]
+    return instances
+
+
+def process_reports(meeting_id: str, new: bool) -> None:
     instances = Zoom().get_meeting_instances(meeting_id)
-    instances = get_localized_instances(instances)
-    meeting_details = Zoom().get_meeting_details(meeting_id)
-    if instances and 'topic' in meeting_details:
+    instances = localize_instances(instances)
+    details = Zoom().get_meeting_details(meeting_id)
+    if instances and details and 'code' not in details:
+        if new:
+            instances = filter_instances(instances)
         for instance in instances:
             report = get_report(instance[0])
             if not report.empty:
-                save_report(report, instance, meeting_details)
+                save_report(report, instance, details)
 
 
 def run() -> None:
@@ -30,6 +45,6 @@ def run() -> None:
     if cli.args.all:
         with Config.meeting_id_file().open('r') as file:
             for meeting_id in file.read().splitlines():
-                process_reports(meeting_id)
+                process_reports(meeting_id, cli.args.new)
     if cli.args.meeting is not None:
-        process_reports(cli.args.meeting)
+        process_reports(cli.args.meeting, cli.args.new)
