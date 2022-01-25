2522db3fbf816d5e6167208b484daf6aed21701d
Jacky Lee <jla524@sfu.ca>
2022-01-25 11:45:35 -0800

    Add class method and static method

--- zoom_report/api/ragic.py
+++ zoom_report/api/ragic.py
@@ -7,29 +7,29 @@ from zoom_report.common.enums import Http, Cogv
 from zoom_report.logger.pkg_logger import Logger
 
 
-def validate_data(data: dict) -> bool:
-    """
-    Ensure the payload data is in the proper format.
-    :param data: a dict with payload data to validate
-    :returns: True if data is valid and False otherwise
-    """
-    if not isinstance(data, dict):
-        return False
-    for key, value in data.items():
-        if not (isinstance(key, (str, int))
-                and isinstance(value, (str, int, float))):
-            return False
-    return True
-
-
 class Ragic:
     """
     Use the requests library to talk to the Ragic API
     """
     _base_url = 'https://na3.ragic.com'
 
+    @staticmethod
+    def validate_data(data: dict) -> bool:
+        """
+        Ensure the payload data is in the proper format.
+        :param data: a dict with payload data to validate
+        :returns: True if data is valid and False otherwise
+        """
+        if not isinstance(data, dict):
+            return False
+        for key, value in data.items():
+            if not (isinstance(key, (str, int))
+                    and isinstance(value, (str, int, float))):
+                return False
+        return True
+
     def _send_data(self, api_route: str, data: dict) -> requests.Response:
-        if not validate_data(data):
+        if not self.validate_data(data):
             raise TypeError("Payload type check failed.")
         url = f'{self._base_url}/{api_route}'
         api_key = Config.ragic_api_key()
--- zoom_report/sdk/transfer_data.py
+++ zoom_report/sdk/transfer_data.py
@@ -9,20 +9,32 @@ class TransferData:
     """
     Use the dropbox library to talk to Dropbox API v2.
     """
-    def __init__(self, access_token):
-        self.access_token = access_token
+    def __init__(self, access_token: str):
+        self.__access_token = access_token
+        self.__overwrite = True
 
-    def upload_file(self, file_from: Path, file_to: Path) -> None:
+    @classmethod
+    def set_overwrite(cls, value: bool) -> None:
+        """
+        Set the value of overwrite
+        :param value: a boolean value
+        :returns: None
+        """
+        cls.__overwrite = value
+
+    def upload_file(self, source: Path, target: Path) -> None:
         """
         Upload a file to Dropbox.
-        :param file_from: a Path of the file to upload
-        :param file_to: a Path in Dropbox to save the file
+        :param source: a Path of the file to upload
+        :param target: a Path in Dropbox to save the file
         :returns: None
         :raises: FileNotFoundError if file_from is not a file
         """
-        if not file_from.is_file():
+        if not source.is_file():
             raise FileNotFoundError("The file to upload does not exist.")
-        dbx = Dropbox(self.access_token)
-        mode = files.WriteMode.overwrite
-        with file_from.open('rb') as file:
-            dbx.files_upload(file.read(), str(file_to), mode=mode)
+        client = Dropbox(self.__access_token)
+        mode = (files.WriteMode.overwrite
+                if self.__overwrite
+                else files.WriteMode.add)
+        with source.open('rb') as file:
+            client.files_upload(file.read(), str(target), mode=mode)
