15c700335859ae43e25400db6545415e308d2e7d
Jacky Lee <jla524@sfu.ca>
2022-01-25 11:53:16 -0800

    Use reST style docstrings

--- zoom_report/__init__.py
+++ zoom_report/__init__.py
@@ -10,7 +10,7 @@ from dotenv import dotenv_values, find_dotenv, set_key
 
 class ThreadSafeMeta(type):
     """
-    This is a thread-safe implementation of Singleton.
+    A thread-safe implementation of Singleton
     """
     _instances: dict = {}
     _lock = Lock()
@@ -18,7 +18,7 @@ class ThreadSafeMeta(type):
     def __call__(cls, *args, **kwargs):
         """
         Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
+          the returned instance
         """
         with cls._lock:
             if cls not in cls._instances:
@@ -29,10 +29,9 @@ class ThreadSafeMeta(type):
 
 class Config(metaclass=ThreadSafeMeta):
     """
-    @description: Global program configuration, uses the dotenv package
-     to load runtime configuration from a .env file, once and
-     only once into this object, this object can be used through-out
-     the code base
+    Global program configuration, uses the dotenv package to load runtime
+      configuration from a .env file, once and only once into this object,
+      this object can be used through-out the code base
     """
     try:
         __package = 'zoom_report'
@@ -63,77 +62,77 @@ class Config(metaclass=ThreadSafeMeta):
     @classmethod
     def package(cls) -> str:
         """
-        @description: getter for package name
+        Getter for package name
         """
         return cls.__package
 
     @classmethod
     def version(cls) -> str:
         """
-        @description: getter for version of package
+        Getter for version of package
         """
         return cls.__version
 
     @classmethod
     def default_env(cls) -> str:
         """
-        @description: getter for the default env
+        Getter for default env
         """
         return cls.__default_env
 
     @classmethod
     def logfile_name(cls) -> str:
         """
-        @description: getter for the logging file name
+        Getter for logging file name
         """
         return cls.__logfile_name
 
     @classmethod
     def env(cls) -> Optional[str]:
         """
-        @description: getter for config
+        Getter for config
         """
         return cls.__env
 
     @classmethod
     def dropbox_api_key(cls) -> Optional[str]:
         """
-        @description: getter for dropbox api key
+        Getter for Dropbox API key
         """
         return cls.__dropbox_api_key
 
     @classmethod
     def ragic_api_key(cls) -> Optional[str]:
         """
-        @description: getter for ragic api key
+        Getter for Ragic API key
         """
         return cls.__ragic_api_key
 
     @classmethod
     def zoom_api_key(cls) -> Optional[str]:
         """
-        @description: getter for zoom api key
+        Getter for Zoom API key
         """
         return cls.__zoom_api_key
 
     @classmethod
     def zoom_api_secret(cls) -> Optional[str]:
         """
-        @description: getter for zoom api secret
+        Getter for Zoom API secret
         """
         return cls.__zoom_api_secret
 
     @classmethod
     def zoom_jwt_token(cls) -> Optional[str]:
         """
-        @description: getter for zoom jwt token
+        Getter for Zoom JWT token
         """
         return cls.__zoom_jwt_token
 
     @classmethod
     def update_jwt_token(cls, new_token: str) -> None:
         """
-        @description: update the expired token with the new one
+        Setter for Zoom JWT token
         """
         if new_token and isinstance(new_token, str):
             set_key(find_dotenv(), cls.__zoom_jwt_token_key, new_token)
@@ -142,62 +141,62 @@ class Config(metaclass=ThreadSafeMeta):
     @classmethod
     def timezone(cls) -> str:
         """
-        @description: getter for timzone
+        Getter for timezone
         """
         return cls.__timezone
 
     @classmethod
     def datetime_format(cls) -> str:
         """
-        @description; getter for datetime format
+        Getter for datetime format
         """
         return cls.__datetime_format
 
     @classmethod
     def config_dir(cls) -> Path:
         """
-        @description: getter for config directory
+        Getter for config directory
         """
         return cls.__config_dir
 
     @classmethod
     def base_dir(cls) -> Path:
         """
-        @description: getter for base directory
+        Getter for base directory
         """
         return cls.__base_dir
 
     @classmethod
     def meeting_id_file(cls) -> Path:
         """
-        @description: getter for meeting id file
+        Getter for meeting id file
         """
         return cls.__meeting_id_file
 
     @classmethod
     def output_dir(cls) -> Path:
         """
-        @description: getter for output directory
+        Getter for output directory
         """
         return cls.__output_dir
 
     @classmethod
     def dropbox_storage_dir(cls) -> Path:
         """
-        @description: getter for dropbox storage directory
+        Getter for dropbox storage directory
         """
         return cls.__dropbox_storage_dir
 
     @classmethod
     def ragic_attendance_route(cls) -> str:
         """
-        @description: getter for ragic attendance route
+        Getter for ragic attendance route
         """
         return cls.__ragic_attendance_route
 
     @classmethod
     def ragic_participants_route(cls) -> str:
         """
-        @description: getter for ragic participants route
+        Getter for ragic participants route
         """
         return cls.__ragic_participants_route
