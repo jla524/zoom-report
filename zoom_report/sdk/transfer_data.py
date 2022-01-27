b5f8d544e69ae7abe0f95b0ea859c6a272a2b1cd
Jacky Lee <jla524@sfu.ca>
2022-01-26 18:29:37 -0800

    Implement proper getters and setters to overwrite

--- zoom_report/sdk/transfer_data.py
+++ zoom_report/sdk/transfer_data.py
@@ -9,18 +9,26 @@ class TransferData:
     """
     Use the dropbox library to talk to Dropbox API v2.
     """
-    def __init__(self, access_token: str):
+    def __init__(self, access_token: str, overwrite: bool = True):
         self.__access_token = access_token
-        self.__overwrite = True
+        self.__overwrite = overwrite
 
-    @classmethod
-    def set_overwrite(cls, value: bool) -> None:
+    @property
+    def overwrite(self) -> bool:
+        """
+        Get the value of overwrite
+        """
+        return self.__overwrite
+
+    @overwrite.setter
+    def overwrite(self, value: bool) -> None:
         """
         Set the value of overwrite
         :param value: a boolean value
         :returns: None
         """
-        cls.__overwrite = value
+        if isinstance(value, bool):
+            self.__overwrite = value
 
     def upload_file(self, source: Path, target: str) -> None:
         """
