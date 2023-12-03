52bee290fea91fcbb14c896208f73fc7d74480b9
Jacky Lee <jla524@sfu.ca>
2023-12-02 21:02:44 -0800

    refactor: move local storage functions

--- zoom_report/automate/storage.py
+++ zoom_report/automate/storage.py
@@ -9,40 +9,10 @@ from pandas import DataFrame
 from zoom_report import Config
 from zoom_report.api.ragic import Ragic
 from zoom_report.sdk.transfer_data import TransferData
+from zoom_report.local.disk import generate_filepath, save_to_disk
 from zoom_report.logger.pkg_logger import Logger
 
 JSON = dict[str, Any]
-
-
-def generate_filepath(topic: str, uuid: str, start_time: str) -> Path:
-    """
-    Generate a file path using a topic, start time, and UUID.
-    :param topic: topic of an instance
-    :param start_time: start time of an instance
-    :param uuid: UUID of an instance
-    :returns: a generated file path
-    """
-    Logger.info("Generating file name...")
-    output_dir = Config.output_dir()
-    output_dir.mkdir(exist_ok=True)
-    date = start_time.split(" ")[0]
-    topic = topic.replace(" ", "-").replace("/", "-")
-    uuid = uuid.replace("/", "-")
-    file_name = f"{topic}_{date}_{uuid}.csv"
-    output_file = output_dir / file_name
-    return output_file
-
-
-def save_csv(frame: DataFrame, file_path: Path) -> None:
-    """
-    Save a given DataFrame as a CSV file.
-    :param frame: a DataFrame to save as CSV
-    :returns: None
-    """
-    Logger.info("Saving report as CSV...")
-    frame.to_csv(file_path, index=False)
-    Logger.info("Report saved in " + str(file_path))
-
 
 def upload_to_dropbox(source_file: Path) -> None:
     """
@@ -92,11 +62,13 @@ def save_report(
     if attendance.empty:
         Logger.warn("Nothing to save, DataFrame is empty.")
         return False
+
     uuid, start_time = instance_info
     path = generate_filepath(meeting_info["topic"], uuid, start_time)
     if path.exists():
         Logger.info("File already exists in storage.")
         return False
+
     payload_info = {
         "uuid": uuid,
         "start_time": start_time,
@@ -105,7 +77,8 @@ def save_report(
     }
     if not write_to_ragic(attendance, payload_info):
         return False
-    save_csv(attendance, path)
+
+    save_to_disk(attendance, path)
     upload_to_dropbox(path)
     Logger.info("Report has been saved in storage.")
     return True
