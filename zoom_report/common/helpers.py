44ffd54432b49b2718fd6161c5a5915fd85ef73f
Jacky Lee <dudu@midnight.local>
2025-12-22 21:27:30 -0800

    feat: dedup on participants level

--- zoom_report/common/helpers.py
+++ zoom_report/common/helpers.py
@@ -1,8 +1,12 @@
 """
 Helper functions
 """
-from typing import Any
+from typing import Any, Optional
 from urllib.parse import quote_plus
+
+import requests
+
+from zoom_report.logger.pkg_logger import Logger
 
 JSON = dict[str, Any]
 Instance = tuple[str, str]
@@ -17,3 +21,20 @@ def encode_uuid(uuid: str) -> str:
     if uuid.startswith("/") or "//" in uuid:
         uuid = quote_plus(quote_plus(uuid))
     return uuid
+
+
+def handle_api_status(response: requests.Reponse, api_description: Optional[str] = None) -> bool:
+    """
+    Parse status message from API response and decide if it's safe to continue.
+    :param response: an API response
+    :return: a boolean value
+    """
+    if not api_description:
+        api_description = "calling the API"
+    if response.get("status", "") == "SKIPPED":
+        Logger.warn("Update has been skipped.")
+        return False
+    if response.get("status", "") == "INVALID":
+        Logger.warn(f"An error occurred when {api_description}.")
+        Logger.error(response["msg"])
+        return False
